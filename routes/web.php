df2a7eb5eda673f603440a09200a051dc6341080
3elik <3ellik@gmail.com>
2023-02-21 21:55:49 +0200

    add index page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CatalogController;
+use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', IndexController::class)->name('index');
 
 Route::get('catalog/index', [CatalogController::class, 'index'])->name('catalog.index');
 Route::get('category/category/{slug}', [CatalogController::class, 'category'])->name('catalog.category');
